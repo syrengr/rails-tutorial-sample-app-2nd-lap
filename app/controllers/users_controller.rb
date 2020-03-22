3ebcde3513e87a6bbdaf64e7608d4c5c01524fe3
lsaqrxq <namasaya.sayuri0412@gmail.co.jp>
2020-03-22 21:16:46 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(1)
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = USer.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
