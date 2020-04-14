9656fbcb85e8074cafc3babccc1cdffc7010ca91
lsaqrxq <namasaya.sayuri0412@gmail.co.jp>
2020-04-14 12:57:39 +0900

    Revert "Add a Static Pages controller"
    This reverts commit 04253863e3369c642521c91cb2eb0f024d28adac.

--- app/controllers/static_paegs_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPaegsController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_paegs/home'
-
-  get 'static_paegs/help'
-
   root 'application#hello'
 end
