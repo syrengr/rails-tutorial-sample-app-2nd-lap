93187f766a74c30a12f22a247655e23609e42b9b
syrengr <namasaya.sayuri0412@gmail.com>
2020-04-24 22:46:01 +0900

    Revert "Implement advanced login"
    This reverts commit abc5c79d8d684b30b0ccde48af8c2bd0942cdc83.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/destroy'
-
   get 'user/new'
 
   get 'users/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200422224138) do
+ActiveRecord::Schema.define(version: 20200418151141) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20200422224138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
