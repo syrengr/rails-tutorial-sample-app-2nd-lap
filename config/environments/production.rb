8f43f990a7b07cecb9c08781235db222bb9e0bb8
syrengr <namasaya.sayuri0412@gmail.com>
2020-04-26 17:42:31 +0900

    Fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'rails-tutorial-sample-2nd-lap.herokuapp.com'
+  host = 'https://rails-tutorial-sample-2nd-lap.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
